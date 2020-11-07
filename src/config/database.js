ac4926e6b8c31ea378367b18f10ac5bd6ac9613b
unknown <francisco@syncode.com.br>
2020-11-06 21:55:52 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: '10.0.0.105',
+    username: 'postgres',
+    password: 'syncode123',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+}
---
+++ src/controllers/AddressController.js
@@ -0,0 +1,34 @@
+const Address = require("../models/Address");
+const User = require("../models/User");
+
+module.exports = {
+    async index(req, res) {
+        
+        const { user_id } = req.params;
+
+        const user = await User.findByPk(user_id, {
+            include: { association: 'addresses' }
+        }); 
+
+        return res.json(user);
+    },
+    
+    async store(req, res) {
+        const { user_id } = req.params;
+        const { zipcode, street, number } = req.body;
+
+        const user = await User.findByPk(user_id);
+
+        if(!user)
+            return res.status(400).json({ error: "Usuário não encontrado "});
+
+        const address = await Address.create({ 
+            zipcode, 
+            user_id, 
+            street, 
+            number 
+        });
+
+        return res.json(address);
+    }
+}
